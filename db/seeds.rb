3f96d15145c98b840a90d46443d38ad19460e16b
JayaS <JayaS@users.noreply.github.com>
2016-12-02 12:01:08 -0600

    Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Mood.destroy_all
+Quote.destroy_all
+
+Mood.create!([{mood_choice: "Disappointment"}, {mood_choice:"Sorrow"}, {mood_choice:"Insult"}, {mood_choice: "Depressed"}, {mood_choice:"Agony"}])
+
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'quotes.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'quptes.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  puts row.to_hash
+  t = Quote.new
+  t.street = row['street']
+  t.body = row['body']
+  t.author = row['author']
+  t.quote_type = row['quote_type']
+  t.save
+  puts "#{t.quote}, #{t.author}, #{t.type} saved"
 end
+
+puts "There are now #{Quote.count} rows in the transactions table"
