6ce774b8ab62aafe953a8f0b1f9a1cdf6822c971
JayaS <JayaS@users.noreply.github.com>
2016-12-05 17:21:33 -0600

    Clearing up routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,22 +2,14 @@ require 'open-uri'
 
 class PagesController < ApplicationController
 
-
   def home
-q = Quote.all.sample
-@quote=q.body
+    q = Quote.all.sample
+    @quote=q.body
 
+    # Using Google api to find lat long from user input address
     if params[:user_street_address].present?
-      # raise params.inspect
       @street_address = params[:user_street_address]
       @street_address_without_spaces = URI.encode(@street_address)
-
-      # ==========================================================================
-      # Your code goes below.
-      # The street address the user input is in the variable @street_address.
-      # A URL-safe version of the street address, with spaces and other illegal
-      #   characters removed, is in the variable @street_address_without_spaces.
-      # ==========================================================================
 
       url="http://maps.googleapis.com/maps/api/geocode/json?address="+@street_address_without_spaces
       parsed_data = JSON.parse(open(url).read)
@@ -26,11 +18,10 @@ q = Quote.all.sample
       @latitude = latitude
       @longitude = longitude
 
+      # Using darksky to get weather for the specific address
       url="https://api.darksky.net/forecast/9d5eb4782ee71dbc0d39bee95b19ccff/#{@latitude},#{@longitude}"
       parsed_data = JSON.parse(open(url).read)
-# raise parsed_data.inspect
       @current_temperature =  parsed_data["currently"]["temperature"]
-
       @current_summary = parsed_data["currently"]["summary"]
       @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
@@ -40,6 +31,6 @@ q = Quote.all.sample
 
     end
 
-      render("pages/home_form.html.erb")
-    end
+    render("pages/home_form.html.erb")
   end
+end
