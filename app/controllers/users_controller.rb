8e9e957777ab5824bca4a45768fb9f4cfe1b3503
JayaS <JayaS@users.noreply.github.com>
2016-12-05 21:58:26 -0600

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,12 @@ class UsersController < ApplicationController
   # skip_before_action :authenticate_user!, :only => [:index, :show]
 
   def show
-    
-    @type=Quote.where(params[:quote_type])
-    @mood=Mood.where(params[:mood_choice])
-    @mood_id=@mood.id
-q= Quote.where(:quote_type => @type).where(:mood_choice =>@mood_id)
+
+    type=Quote.where(params[:quote_type])
+    mood=Mood.where(params[:mood_choice])
+    mood_id=mood.id
+q= Quote.where(:quote_type => type,:mood_choice =>mood_id)
 @quote = q.body
 render("pages/home_form.html.erb")
   end
-
-
 end
